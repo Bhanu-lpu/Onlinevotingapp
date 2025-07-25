537bc346a01aa81a331133ac0fdba7da9f83938f
Ch.Bhanu sesha sai <167855945+Bhanu-lpu@users.noreply.github.com>
2025-07-25 23:23:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -150,5 +150,6 @@ def clear_votes():
 
 
 # === Run App ===
-if __name__ == '__main__':
-    app.run(debug=True, port=10000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)), debug=True)
+
